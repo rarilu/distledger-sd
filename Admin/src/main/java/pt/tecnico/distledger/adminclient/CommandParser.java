89926dc6c94bed4a01506c7529c3f4d75c367d74
Luís Fonseca <luis@lhf.pt>
2023-03-03 10:59:13 +0000

    fix: formatting

--- Admin/src/main/java/pt/tecnico/distledger/adminclient/CommandParser.java
+++ Admin/src/main/java/pt/tecnico/distledger/adminclient/CommandParser.java
@@ -1,113 +1,114 @@
 package pt.tecnico.distledger.adminclient;
 
-import pt.tecnico.distledger.adminclient.grpc.AdminService;
-
 import java.util.Scanner;
+import pt.tecnico.distledger.adminclient.grpc.AdminService;
 
 public class CommandParser {
 
-    private static final String SPACE = " ";
-    private static final String ACTIVATE = "activate";
-    private static final String DEACTIVATE = "deactivate";
-    private static final String GET_LEDGER_STATE = "getLedgerState";
-    private static final String GOSSIP = "gossip";
-    private static final String HELP = "help";
-    private static final String EXIT = "exit";
+  private static final String SPACE = " ";
+  private static final String ACTIVATE = "activate";
+  private static final String DEACTIVATE = "deactivate";
+  private static final String GET_LEDGER_STATE = "getLedgerState";
+  private static final String GOSSIP = "gossip";
+  private static final String HELP = "help";
+  private static final String EXIT = "exit";
 
-    private final AdminService adminService;
-    public CommandParser(AdminService adminService) {
-        this.adminService = adminService;
+  private final AdminService adminService;
+
+  public CommandParser(AdminService adminService) {
+    this.adminService = adminService;
+  }
+
+  void parseInput() {
+
+    Scanner scanner = new Scanner(System.in);
+    boolean exit = false;
+
+    while (!exit) {
+      System.out.print("> ");
+      String line = scanner.nextLine().trim();
+      String cmd = line.split(SPACE)[0];
+
+      switch (cmd) {
+        case ACTIVATE:
+          this.activate(line);
+          break;
+
+        case DEACTIVATE:
+          this.deactivate(line);
+          break;
+
+        case GET_LEDGER_STATE:
+          this.dump(line);
+          break;
+
+        case GOSSIP:
+          this.gossip(line);
+          break;
+
+        case HELP:
+          this.printUsage();
+          break;
+
+        case EXIT:
+          exit = true;
+          break;
+
+        default:
+          break;
+      }
     }
-    void parseInput() {
+  }
 
-        Scanner scanner = new Scanner(System.in);
-        boolean exit = false;
+  private void activate(String line) {
+    String[] split = line.split(SPACE);
 
-        while (!exit) {
-            System.out.print("> ");
-            String line = scanner.nextLine().trim();
-            String cmd = line.split(SPACE)[0];
-
-            switch (cmd) {
-                case ACTIVATE:
-                    this.activate(line);
-                    break;
-
-                case DEACTIVATE:
-                    this.deactivate(line);
-                    break;
-
-                case GET_LEDGER_STATE:
-                    this.dump(line);
-                    break;
-
-                case GOSSIP:
-                    this.gossip(line);
-                    break;
-
-                case HELP:
-                    this.printUsage();
-                    break;
-
-                case EXIT:
-                    exit = true;
-                    break;
-
-                default:
-                    break;
-            }
-
-        }
+    if (split.length != 2) {
+      this.printUsage();
+      return;
     }
+    String server = split[1];
 
-    private void activate(String line){
-        String[] split = line.split(SPACE);
+    System.out.println("TODO: implement activate command");
+  }
 
-        if (split.length != 2){
-            this.printUsage();
-            return;
-        }
-        String server = split[1];
+  private void deactivate(String line) {
+    String[] split = line.split(SPACE);
 
-        System.out.println("TODO: implement activate command");
+    if (split.length != 2) {
+      this.printUsage();
+      return;
     }
+    String server = split[1];
 
-    private void deactivate(String line){
-        String[] split = line.split(SPACE);
+    System.out.println("TODO: implement deactivate command");
+  }
 
-        if (split.length != 2){
-            this.printUsage();
-            return;
-        }
-        String server = split[1];
+  private void dump(String line) {
+    String[] split = line.split(SPACE);
 
-        System.out.println("TODO: implement deactivate command");
+    if (split.length != 2) {
+      this.printUsage();
+      return;
     }
+    String server = split[1];
 
-    private void dump(String line){
-        String[] split = line.split(SPACE);
+    System.out.println("TODO: implement getLedgerState command");
+  }
 
-        if (split.length != 2){
-            this.printUsage();
-            return;
-        }
-        String server = split[1];
+  @SuppressWarnings("unused")
+  private void gossip(String line) {
+    /* TODO Phase-3 */
+    System.out.println("TODO: implement gossip command (only for Phase-3)");
+  }
 
-        System.out.println("TODO: implement getLedgerState command");
-    }
-
-    @SuppressWarnings("unused")
-    private void gossip(String line){
-        /* TODO Phase-3 */
-        System.out.println("TODO: implement gossip command (only for Phase-3)");
-    }
-    private void printUsage() {
-        System.out.println("Usage:\n" +
-                "- activate <server>\n" +
-                "- deactivate <server>\n" +
-                "- getLedgerState <server>\n" +
-                "- gossip <server>\n" +
-                "- exit\n");
-    }
-
+  private void printUsage() {
+    System.out.println(
+        "Usage:\n"
+            + "- activate <server>\n"
+            + "- deactivate <server>\n"
+            + "- getLedgerState <server>\n"
+            + "- gossip <server>\n"
+            + "- exit\n");
+  }
 }
--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/ServerState.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/ServerState.java
@@ -1,18 +1,17 @@
 package pt.tecnico.distledger.server.domain;
 
-import pt.tecnico.distledger.server.domain.operation.Operation;
-
 import java.util.ArrayList;
 import java.util.List;
+import pt.tecnico.distledger.server.domain.operation.Operation;
 
 public class ServerState {
-    private List<Operation> ledger;
+  private List<Operation> ledger;
 
-    public ServerState() {
-        this.ledger = new ArrayList<>();
-    }
+  public ServerState() {
+    this.ledger = new ArrayList<>();
+  }
 
-    /* TODO: Here should be declared all the server state attributes
-         as well as the methods to access and interact with the state. */
+  /* TODO: Here should be declared all the server state attributes
+  as well as the methods to access and interact with the state. */
 
 }
