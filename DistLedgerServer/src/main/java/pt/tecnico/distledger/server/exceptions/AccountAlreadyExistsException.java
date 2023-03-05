81c7bfb3be0317b5e27dc0d879dd1dda32b708fa
Ricardo Antunes <me@riscadoa.com>
2023-03-05 19:05:47 +0000

    feat: add OperationException

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/exceptions/AccountAlreadyExistsException.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/exceptions/AccountAlreadyExistsException.java
@@ -1,6 +1,6 @@
 package pt.tecnico.distledger.server.exceptions;
 
-public class AccountAlreadyExistsException extends RuntimeException {
+public class AccountAlreadyExistsException extends OperationException {
   public AccountAlreadyExistsException(String userId) {
     super("Account for user " + userId + " already exists");
   }
