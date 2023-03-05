1de44d72311dcad9395a0e83c58a26d15fa25a36
Ricardo Antunes <me@riscadoa.com>
2023-03-05 18:09:34 +0000

    feat: setup ServerMain.java

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerMain.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/ServerMain.java
@@ -1,7 +1,35 @@
 package pt.tecnico.distledger.server;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import java.io.IOException;
+
 public class ServerMain {
-  public static void main(String[] args) {
-    /* TODO */
+  public static void main(String[] args) throws IOException, InterruptedException {
+    System.out.println(ServerMain.class.getSimpleName());
+
+    // Receive and print arguments.
+    System.out.printf("Received %d arguments%n", args.length);
+    for (int i = 0; i < args.length; i++) {
+      System.out.printf("arg[%d] = %s%n", i, args[i]);
+    }
+
+    // Check arguments.
+    if (args.length != 2) {
+      System.err.println("Argument(s) missing!");
+      System.err.println("Usage: mvn exec:java -Dexec.args=<port> <qual>");
+      return;
+    }
+
+    final int port = Integer.parseInt(args[0]);
+    final String qualifier = args[1];
+
+    // Launch server.
+    Server server = ServerBuilder.forPort(port).build();
+    server.start();
+    System.out.println("Server started, listening on " + port);
+
+    // Wait until server is terminated.
+    server.awaitTermination();
   }
 }
