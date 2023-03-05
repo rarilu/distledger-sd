0a02b3ecec53142a15192a0fd1e383d69579d6e5
Ricardo Antunes <me@riscadoa.com>
2023-03-05 18:52:40 +0000

    feat: add accounts & operation adder to ServerState

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/ServerState.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/ServerState.java
@@ -1,16 +1,25 @@
 package pt.tecnico.distledger.server.domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import pt.tecnico.distledger.server.domain.operation.Operation;
 
 public class ServerState {
-  private List<Operation> ledger;
+  private List<Operation> ledger = new ArrayList<>();
+  private Map<String, Integer> accounts = new HashMap<>();
 
   public ServerState() {
     this.ledger = new ArrayList<>();
   }
 
-  /* TODO: Here should be declared all the server state attributes
-  as well as the methods to access and interact with the state. */
+  public void add(Operation op) {
+    ledger.add(op);
+    op.apply(this);
+  }
+
+  public Map<String, Integer> getAccounts() {
+    return accounts;
+  }
 }
