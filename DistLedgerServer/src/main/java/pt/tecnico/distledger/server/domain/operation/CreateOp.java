9ee4d4f33d170e4d7eb720f4dc587861ceeb794d
Ricardo Antunes <me@riscadoa.com>
2023-03-05 18:46:16 +0000

    feat: add apply abstract method to Operation

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/operation/CreateOp.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/operation/CreateOp.java
@@ -1,7 +1,14 @@
 package pt.tecnico.distledger.server.domain.operation;
 
+import pt.tecnico.distledger.server.domain.ServerState;
+
 public class CreateOp extends Operation {
   public CreateOp(String account) {
     super(account);
+  }
+
+  @Override
+  public void apply(ServerState state) {
+    // TODO
   }
 }
--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/operation/DeleteOp.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/operation/DeleteOp.java
@@ -1,7 +1,14 @@
 package pt.tecnico.distledger.server.domain.operation;
 
+import pt.tecnico.distledger.server.domain.ServerState;
+
 public class DeleteOp extends Operation {
   public DeleteOp(String account) {
     super(account);
+  }
+
+  @Override
+  public void apply(ServerState state) {
+    // TODO
   }
 }
