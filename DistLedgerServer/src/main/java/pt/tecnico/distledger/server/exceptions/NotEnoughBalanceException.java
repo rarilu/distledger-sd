1babcb44a242c1c6b32a4a4faa755703735ab3a4
Ricardo Antunes <me@riscadoa.com>
2023-03-06 11:25:56 +0000

    feat: add NotEnoughBalanceException

---
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/exceptions/NotEnoughBalanceException.java
@@ -0,0 +1,11 @@
+package pt.tecnico.distledger.server.exceptions;
+
+import io.grpc.Status;
+
+public class NotEnoughBalanceException extends OperationException {
+  public NotEnoughBalanceException(String account, int amount) {
+    super(
+        Status.FAILED_PRECONDITION.withDescription(
+            "Account " + account + " does not have enough balance to transfer " + amount));
+  }
+}
