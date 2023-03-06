338f97c5904b4f72e8e813c5bec7546201bc5f1d
Ricardo Antunes <me@riscadoa.com>
2023-03-06 15:23:41 +0000

    feat: implement UserServiceImpl.transferTo

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/UserServiceImpl.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/UserServiceImpl.java
@@ -4,8 +4,11 @@ import io.grpc.stub.StreamObserver;
 import pt.tecnico.distledger.server.domain.ServerState;
 import pt.tecnico.distledger.server.domain.exceptions.OperationException;
 import pt.tecnico.distledger.server.domain.operation.CreateOp;
+import pt.tecnico.distledger.server.domain.operation.TransferOp;
 import pt.ulisboa.tecnico.distledger.contract.user.UserDistLedger.CreateAccountRequest;
 import pt.ulisboa.tecnico.distledger.contract.user.UserDistLedger.CreateAccountResponse;
+import pt.ulisboa.tecnico.distledger.contract.user.UserDistLedger.TransferToRequest;
+import pt.ulisboa.tecnico.distledger.contract.user.UserDistLedger.TransferToResponse;
 import pt.ulisboa.tecnico.distledger.contract.user.UserServiceGrpc;
 
 public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
@@ -21,6 +24,19 @@ public class UserServiceImpl extends UserServiceGrpc.UserServiceImplBase {
     try {
       this.state.registerOperation(new CreateOp(request.getUserId()));
       responseObserver.onNext(CreateAccountResponse.getDefaultInstance());
+      responseObserver.onCompleted();
+    } catch (OperationException e) {
+      responseObserver.onError(e.getGrpcStatus().asRuntimeException());
+    }
+  }
+
+  @Override
+  public void transferTo(
+      TransferToRequest request, StreamObserver<TransferToResponse> responseObserver) {
+    try {
+      state.registerOperation(
+          new TransferOp(request.getAccountFrom(), request.getAccountTo(), request.getAmount()));
+      responseObserver.onNext(TransferToResponse.getDefaultInstance());
       responseObserver.onCompleted();
     } catch (OperationException e) {
       responseObserver.onError(e.getGrpcStatus().asRuntimeException());
