8b53ae999e5b8ce843cf1d741cadce0f7a5d9bf5
Ricardo Antunes <me@riscadoa.com>
2023-03-06 11:03:48 +0000

    test: add create account tests

--- DistLedgerServer/src/test/groovy/pt/tecnico/distledeger/server/ServerStateTest.groovy
+++ DistLedgerServer/src/test/groovy/pt/tecnico/distledeger/server/ServerStateTest.groovy
@@ -1,6 +1,8 @@
 import spock.lang.Specification
 
 import pt.tecnico.distledger.server.domain.ServerState
+import pt.tecnico.distledger.server.domain.operation.CreateOp
+import pt.tecnico.distledger.server.exceptions.AccountAlreadyExistsException
 
 class ServerStateTest extends Specification {
     def "state has broker account with 1000"() {
@@ -10,5 +12,31 @@ class ServerStateTest extends Specification {
         then:
         state.getAccounts().size() == 1
         state.getAccounts().get("broker") == 1000
+    }
+
+    def "create a new account"() {
+        given:
+        def state = new ServerState()
+
+        when:
+        state.add(new CreateOp("alice"))
+
+        then:
+        state.getAccounts().size() == 2
+        state.getAccounts().get("alice") == 0
+    }
+
+    def "create a duplicate account"() {
+        given:
+        def state = new ServerState()
+        state.add(new CreateOp("alice"))
+
+        when:
+        state.add(new CreateOp("alice"))
+
+        then:
+        def e = thrown(AccountAlreadyExistsException)
+        e.getGrpcStatus().getCode() == io.grpc.Status.ALREADY_EXISTS.getCode()
+        state.getAccounts().size() == 2
     }
 }
