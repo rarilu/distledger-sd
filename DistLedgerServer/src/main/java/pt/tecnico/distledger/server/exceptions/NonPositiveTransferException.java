09e26010a1c812a5c3973a7c111ca3fc0741879a
Ricardo Antunes <me@riscadoa.com>
2023-03-06 11:47:10 +0000

    feat: add NonPositiveTransferException

---
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/exceptions/NonPositiveTransferException.java
@@ -0,0 +1,11 @@
+package pt.tecnico.distledger.server.exceptions;
+
+import io.grpc.Status;
+
+public class NonPositiveTransferException extends OperationException {
+  public NonPositiveTransferException() {
+    super(
+        Status.INVALID_ARGUMENT.withDescription(
+            "Transfers with non-positive amount are not allowed"));
+  }
+}
