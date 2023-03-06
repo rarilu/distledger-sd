613fa2a38fa832c952dd360cca2b2c7cfd657abf
Ricardo Antunes <me@riscadoa.com>
2023-03-06 11:11:52 +0000

    feat: add debug traces to CreateOp

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/operation/CreateOp.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/operation/CreateOp.java
@@ -3,6 +3,7 @@ package pt.tecnico.distledger.server.domain.operation;
 import pt.tecnico.distledger.server.domain.ServerState;
 import pt.tecnico.distledger.server.exceptions.AccountAlreadyExistsException;
 import pt.tecnico.distledger.server.exceptions.OperationException;
+import pt.tecnico.distledger.utils.Logger;
 
 public class CreateOp extends Operation {
   public CreateOp(String account) {
@@ -12,9 +13,11 @@ public class CreateOp extends Operation {
   @Override
   public void apply(ServerState state) throws OperationException {
     if (state.getAccounts().containsKey(this.getAccount())) {
+      Logger.debug("Account " + this.getAccount() + " already exists");
       throw new AccountAlreadyExistsException(this.getAccount());
     } else {
       state.getAccounts().put(this.getAccount(), 0);
+      Logger.debug("Created account " + this.getAccount());
     }
   }
 }
