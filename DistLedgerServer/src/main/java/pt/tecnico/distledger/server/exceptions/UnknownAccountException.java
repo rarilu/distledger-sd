a9d1aa80b2395a0fdcb833f897d7e9070de8b30d
Ricardo Antunes <me@riscadoa.com>
2023-03-06 11:21:28 +0000

    feat: add UnknownAccountException

---
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/exceptions/UnknownAccountException.java
@@ -0,0 +1,9 @@
+package pt.tecnico.distledger.server.exceptions;
+
+import io.grpc.Status;
+
+public class UnknownAccountException extends OperationException {
+  public UnknownAccountException(String account) {
+    super(Status.NOT_FOUND.withDescription("Account " + account + " does not exist"));
+  }
+}
