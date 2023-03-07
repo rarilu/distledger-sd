8d6425c2d9d93bb3886f8c2c17b5a5a85d5c33e3
Ricardo Antunes <me@riscadoa.com>
2023-03-07 21:13:24 +0000

    test(Admin): add ActivateTest

---
+++ Admin/src/test/groovy/pt/tecnico/distledger/adminclient/ActivateTest.groovy
@@ -0,0 +1,73 @@
+package pt.tecnico.distledger.adminclient
+
+import io.grpc.Status
+import pt.ulisboa.tecnico.distledger.contract.admin.AdminServiceGrpc
+import pt.ulisboa.tecnico.distledger.contract.admin.AdminDistLedger.ActivateRequest
+import pt.ulisboa.tecnico.distledger.contract.admin.AdminDistLedger.ActivateResponse
+import org.grpcmock.GrpcMock
+
+class ActivateTest extends BaseTest {
+    def "user provides invalid activate command"() {
+        given: "an invalid activate input"
+        provideInput("activate wrong usage\nexit\n")
+
+        when: "the admin client is run"
+        runMain()
+
+        then: "the output is correct"
+        outBuf.toString() == ("> " + EXPECTED_USAGE_STRING + "\n> ")
+
+        and: "the mock server received no requests"
+        GrpcMock.verifyThat(GrpcMock.calledMethod(AdminServiceGrpc.getActivateMethod()), GrpcMock.never())
+    }
+
+    def "activate server returns empty response"() {
+        given: "a activate input"
+        provideInput("activate A\nexit\n")
+
+        and: "a mock server that returns an empty response"
+        GrpcMock.stubFor(
+                GrpcMock.unaryMethod(AdminServiceGrpc.getActivateMethod())
+                        .willReturn(GrpcMock.response(ActivateResponse.getDefaultInstance())))
+
+        when: "the admin client is run"
+        runMain()
+
+        then: "the output is correct"
+        outBuf.toString() == "> OK\n\n> "
+
+        and: "the mock server received the correct request, exactly once"
+        GrpcMock.verifyThat(
+                GrpcMock.calledMethod(AdminServiceGrpc.getActivateMethod())
+                        .withRequest(ActivateRequest.getDefaultInstance()),
+                GrpcMock.times(1)
+        )
+    }
+
+    def "activate returns a status exception"() {
+        given: "a create account input"
+        provideInput("activate A\nexit\n")
+
+        and: "a mock server that returns a status exception"
+        GrpcMock.stubFor(
+                GrpcMock.unaryMethod(AdminServiceGrpc.getActivateMethod())
+                        .willReturn(GrpcMock.statusException(
+                                Status.UNKNOWN
+                                        .withDescription("The server raised an exception")
+                        ))
+        )
+
+        when: "the admin client is run"
+        runMain()
+
+        then: "the output is correct"
+        outBuf.toString() == "> Error: The server raised an exception\n\n> "
+
+        and: "the mock server received the correct request, exactly once"
+        GrpcMock.verifyThat(
+                GrpcMock.calledMethod(AdminServiceGrpc.getActivateMethod())
+                        .withRequest(ActivateRequest.getDefaultInstance()),
+                GrpcMock.times(1)
+        )
+    }
+}
