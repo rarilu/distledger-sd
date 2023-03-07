3b1cfd67e627523ac345f3f20445895da63203c4
Ricardo Antunes <me@riscadoa.com>
2023-03-07 13:51:42 +0000

    fix: add explanatory labels to ServerStateTest

--- DistLedgerServer/src/test/groovy/pt/tecnico/distledeger/server/ServerStateTest.groovy
+++ DistLedgerServer/src/test/groovy/pt/tecnico/distledeger/server/ServerStateTest.groovy
@@ -6,37 +6,45 @@ import pt.tecnico.distledger.server.exceptions.AccountAlreadyExistsException
 
 class ServerStateTest extends Specification {
     def "state has broker account with 1000"() {
-        when:
+        when: "a new server state is created"
         def state = new ServerState()
 
-        then:
-        state.getAccounts().size() == 1
+        then: "exactly one account exists"
+        state.getAccounts().size() == 1        
+
+        and: "the broker account has the correct balance"
         state.getAccounts().get("broker") == 1000
     }
 
     def "create a new account"() {
-        given:
+        given: "a new server state"
         def state = new ServerState()
 
-        when:
+        when: "a new account is created"
         state.registerOperation(new CreateOp("alice"))
 
-        then:
+        then: "there are exactly two accounts"
         state.getAccounts().size() == 2
+
+        and: "the new account has the correct balance"
         state.getAccounts().get("alice") == 0
     }
 
     def "create a duplicate account"() {
-        given:
+        given: "a new server state"
         def state = new ServerState()
+
+        and: "with an account already created"
         state.registerOperation(new CreateOp("alice"))
 
-        when:
+        when: "an account with the same name is created"
         state.registerOperation(new CreateOp("alice"))
 
-        then:
+        then: "an exception is thrown"
         def e = thrown(AccountAlreadyExistsException)
         e.getGrpcStatus().getCode() == io.grpc.Status.ALREADY_EXISTS.getCode()
+
+        and: "the number of accounts is still 2"
         state.getAccounts().size() == 2
     }
 }
