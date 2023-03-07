0bb64da8dfd1c8d74466b50b85b88095c198fe8c
Rafael Oliveira <rafdev.x@gmail.com>
2023-03-07 13:27:10 +0000

    test: add coverage to when user provides unknown command

--- User/src/test/groovy/pt/tecnico/distledger/userclient/UserClientMainTest.groovy
+++ User/src/test/groovy/pt/tecnico/distledger/userclient/UserClientMainTest.groovy
@@ -1,5 +1,8 @@
 package pt.tecnico.distledger.userclient
 
+import org.grpcmock.GrpcMock
+import pt.ulisboa.tecnico.distledger.contract.user.UserServiceGrpc
+
 class UserClientMainTest extends BaseTest {
     def "main is called with no arguments"() {
         when: "the user client is called with no arguments"
@@ -11,5 +14,19 @@ class UserClientMainTest extends BaseTest {
 
         and: "an error is logged"
         errBuf.toString() == "Argument(s) missing!\nUsage: mvn exec:java -Dexec.args=<host> <port>\n"
+    }
+
+    def "user provides an unknown command"() {
+        given: "an unknown command input"
+        provideInput("thisCommandDoesNotExist wrong arguments\nexit\n")
+
+        when: "the user client is run"
+        runMain()
+
+        then: "the output is correct"
+        outBuf.toString() == ("> " + EXPECTED_USAGE_STRING + "\n> ")
+
+        and: "no errors were logged"
+        errBuf.toString() == ""
     }
 }
