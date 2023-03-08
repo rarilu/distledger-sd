94626739f677234796b304a41698879117cf2fcd
Ricardo Antunes <me@riscadoa.com>
2023-03-08 12:31:34 +0000

    feat(Server): implement AdminServiceImpl.getLedgerState

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/AdminServiceImpl.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/AdminServiceImpl.java
@@ -1,6 +1,11 @@
 package pt.tecnico.distledger.server;
 
+import io.grpc.stub.StreamObserver;
 import pt.tecnico.distledger.server.domain.ServerState;
+import pt.tecnico.distledger.server.domain.exceptions.OperationException;
+import pt.tecnico.distledger.server.visitors.LedgerStateGenerator;
+import pt.ulisboa.tecnico.distledger.contract.admin.AdminDistLedger.getLedgerStateRequest;
+import pt.ulisboa.tecnico.distledger.contract.admin.AdminDistLedger.getLedgerStateResponse;
 import pt.ulisboa.tecnico.distledger.contract.admin.AdminServiceGrpc;
 
 public class AdminServiceImpl extends AdminServiceGrpc.AdminServiceImplBase {
@@ -8,5 +13,18 @@ public class AdminServiceImpl extends AdminServiceGrpc.AdminServiceImplBase {
 
   public AdminServiceImpl(ServerState state) {
     this.state = state;
+  }
+
+  @Override
+  public void getLedgerState(
+      getLedgerStateRequest request, StreamObserver<getLedgerStateResponse> responseObserver) {
+    LedgerStateGenerator generator = new LedgerStateGenerator();
+
+    this.state.visitLedger(generator);
+
+    getLedgerStateResponse response =
+        getLedgerStateResponse.newBuilder().setLedgerState(generator.build()).build();
+    responseObserver.onNext(response);
+    responseObserver.onCompleted();
   }
 }
