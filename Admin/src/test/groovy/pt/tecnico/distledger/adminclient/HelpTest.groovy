f67502ea4ab91cff59a95916e59216b54580ddd0
Ricardo Antunes <me@riscadoa.com>
2023-03-08 18:14:33 +0000

    test(Admin): add HelpTest

---
+++ Admin/src/test/groovy/pt/tecnico/distledger/adminclient/HelpTest.groovy
@@ -0,0 +1,21 @@
+package pt.tecnico.distledger.adminclient
+
+class HelpTest extends BaseTest {
+    def EXPECTED_HELP_STRING = "Usage:\n" +
+        "- activate <server>\n" +
+        "- deactivate <server>\n" +
+        "- getLedgerState <server>\n" +
+        "- gossip <server>\n" +
+        "- exit\n"
+
+    def "help output is correct"() {
+        given: "a help command"
+        provideInput("help")
+
+        when: "the admin client is run"
+        runMain()
+
+        then: "the output is correct"
+        outBuf.toString() == ("> " + EXPECTED_HELP_STRING + "\n> ")
+    }
+}
