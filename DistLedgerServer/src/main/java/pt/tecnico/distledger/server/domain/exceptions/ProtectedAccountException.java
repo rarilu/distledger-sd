d7601525185094a836f9016ce4f1391e96b83779
Luís Fonseca <luis@lhf.pt>
2023-03-08 13:56:55 +0000

    fix(DistLedgerServer): ProtectedAccountException should use INVALID_ARGUMENT status

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/exceptions/ProtectedAccountException.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/exceptions/ProtectedAccountException.java
@@ -4,7 +4,6 @@ import io.grpc.Status;
 
 public class ProtectedAccountException extends OperationException {
   public ProtectedAccountException(String userId) {
-    super(
-        Status.FAILED_PRECONDITION.withDescription("Account for user " + userId + " is protected"));
+    super(Status.INVALID_ARGUMENT.withDescription("Account for user " + userId + " is protected"));
   }
 }
