e1678e9a4f06de12977ac5cb72094f28f8e3394e
Luís Fonseca <luis@lhf.pt>
2023-03-08 09:59:09 +0000

    test(DistLedgerServer): test DeleteOp

---
+++ DistLedgerServer/src/test/groovy/pt/tecnico/distledeger/server/operations/DeleteOpTest.groovy
@@ -0,0 +1,53 @@
+import spock.lang.Specification
+
+import pt.tecnico.distledger.server.domain.ServerState
+import pt.tecnico.distledger.server.domain.operation.CreateOp
+import pt.tecnico.distledger.server.domain.operation.DeleteOp
+import pt.tecnico.distledger.server.domain.exceptions.UnknownAccountException
+import pt.tecnico.distledger.server.domain.exceptions.NonEmptyAccountException
+
+class DeleteOpTest extends Specification {
+    def "delete an account"() {
+        given: "a new server state"
+        def state = new ServerState()
+
+        and: "with an account already created"
+        state.registerOperation(new CreateOp("Alice"))
+
+        when: "the account is deleted"
+        state.registerOperation(new DeleteOp("Alice"))
+
+        then: "there is only one account"
+        state.getAccounts().size() == 1
+    }
+
+    def "delete a non-existing account"() {
+        given: "a new server state"
+        def state = new ServerState()
+
+        when: "an unknown account is deleted"
+        state.registerOperation(new DeleteOp("Alice"))
+
+        then: "an exception is thrown"
+        def e = thrown(UnknownAccountException)
+        e.getGrpcStatus().getCode() == io.grpc.Status.NOT_FOUND.getCode()
+    }
+
+    def "delete a non-empty account"() {
+        given: "a new server state"
+        def state = new ServerState()
+
+        and: "with an account already created"
+        state.registerOperation(new CreateOp("Alice"))
+
+        and: "with a balance in the account"
+        state.getAccounts().get("Alice").setBalance(10)
+
+        when: "the account is deleted"
+        state.registerOperation(new DeleteOp("Alice"))
+
+        then: "an exception is thrown"
+        def e = thrown(NonEmptyAccountException)
+        e.getGrpcStatus().getCode() == io.grpc.Status.FAILED_PRECONDITION.getCode()
+    }
+}
