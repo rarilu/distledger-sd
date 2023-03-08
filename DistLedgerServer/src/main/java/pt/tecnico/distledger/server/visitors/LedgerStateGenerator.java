4fa9f9af54c3a4c8696f1bd3bf7e82e88b7a3c13
Ricardo Antunes <me@riscadoa.com>
2023-03-08 12:26:18 +0000

    feat(Server): add LedgerStateGenerator

---
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/visitors/LedgerStateGenerator.java
@@ -0,0 +1,49 @@
+package pt.tecnico.distledger.server.visitors;
+
+import pt.tecnico.distledger.server.domain.operation.CreateOp;
+import pt.tecnico.distledger.server.domain.operation.DeleteOp;
+import pt.tecnico.distledger.server.domain.operation.TransferOp;
+import pt.ulisboa.tecnico.distledger.contract.DistLedgerCommonDefinitions.LedgerState;
+import pt.ulisboa.tecnico.distledger.contract.DistLedgerCommonDefinitions.Operation;
+import pt.ulisboa.tecnico.distledger.contract.DistLedgerCommonDefinitions.OperationType;
+
+public class LedgerStateGenerator extends OperationVisitor {
+  private LedgerState.Builder builder;
+
+  public LedgerStateGenerator() {
+    this.builder = LedgerState.newBuilder();
+  }
+
+  public LedgerState build() {
+    return this.builder.build();
+  }
+
+  @Override
+  public void visit(CreateOp op) {
+    this.builder.addLedger(
+        Operation.newBuilder()
+            .setType(OperationType.OP_CREATE_ACCOUNT)
+            .setUserId(op.getUserId())
+            .build());
+  }
+
+  @Override
+  public void visit(DeleteOp op) {
+    this.builder.addLedger(
+        Operation.newBuilder()
+            .setType(OperationType.OP_DELETE_ACCOUNT)
+            .setUserId(op.getUserId())
+            .build());
+  }
+
+  @Override
+  public void visit(TransferOp op) {
+    this.builder.addLedger(
+        Operation.newBuilder()
+            .setType(OperationType.OP_TRANSFER_TO)
+            .setUserId(op.getUserId())
+            .setDestUserId(op.getDestUserId())
+            .setAmount(op.getAmount())
+            .build());
+  }
+}
