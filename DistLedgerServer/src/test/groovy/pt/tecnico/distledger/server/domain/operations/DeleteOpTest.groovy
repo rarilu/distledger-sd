613cad620094201e34d0a95f3922cc280dbdbcfa
Ricardo Antunes <me@riscadoa.com>
2023-03-10 17:01:44 +0000

    test(Server): concurrency DeleteOp edge case

--- DistLedgerServer/src/test/groovy/pt/tecnico/distledger/server/domain/operations/DeleteOpTest.groovy
+++ DistLedgerServer/src/test/groovy/pt/tecnico/distledger/server/domain/operations/DeleteOpTest.groovy
@@ -1,7 +1,8 @@
 package pt.tecnico.distledger.server.domain.operations
 
 import spock.lang.Specification
-
+import java.util.concurrent.ConcurrentMap
+import pt.tecnico.distledger.server.domain.Account
 import pt.tecnico.distledger.server.domain.ServerState
 import pt.tecnico.distledger.server.domain.operation.CreateOp
 import pt.tecnico.distledger.server.domain.operation.DeleteOp
@@ -65,5 +66,27 @@ class DeleteOpTest extends Specification {
 
         and: "the account still exists"
         state.getAccounts().containsKey("Alice")
+    }
+
+    def "delete before entering the sync block"() {
+        given: "a mock state"
+        def state = Mock(ServerState)
+        def accounts = Mock(ConcurrentMap)
+        state.getAccounts() >> accounts
+
+        and: "an executor with the mock state"
+        def executor = new OperationExecutor(state)
+        
+        and: "first call to get returns an account"
+        1 * accounts.get("Alice") >> new Account()
+
+        and: "first call to containsKey returns false"
+        1 * accounts.containsKey("Alice") >> false
+
+        when: "the account is deleted"
+        executor.execute(new DeleteOp("Alice"))
+
+        then: "an exception is thrown"
+        thrown(UnknownAccountException)
     }
 }
