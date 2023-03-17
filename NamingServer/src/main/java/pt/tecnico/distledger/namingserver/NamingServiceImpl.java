f112c0e02f7f4a28acb27bf1e9d021cd2380a664
Rafael Oliveira <rafdev.x@gmail.com>
2023-03-17 11:17:38 +0000

    feat(NamingServer): implement register Naming Service operation
    Closes #110

--- NamingServer/src/main/java/pt/tecnico/distledger/namingserver/NamingServiceImpl.java
+++ NamingServer/src/main/java/pt/tecnico/distledger/namingserver/NamingServiceImpl.java
@@ -1,13 +1,30 @@
 package pt.tecnico.distledger.namingserver;
 
+import io.grpc.Status;
+import io.grpc.stub.StreamObserver;
+import pt.tecnico.distledger.contract.namingserver.NamingServerDistLedger.RegisterRequest;
+import pt.tecnico.distledger.contract.namingserver.NamingServerDistLedger.RegisterResponse;
 import pt.tecnico.distledger.contract.namingserver.NamingServiceGrpc;
 import pt.tecnico.distledger.namingserver.domain.NamingServerState;
+import pt.tecnico.distledger.utils.Logger;
 
 /** Implements the Admin service, handling gRPC requests. */
 public class NamingServiceImpl extends NamingServiceGrpc.NamingServiceImplBase {
+  private static final String REGISTER_FAILED = "Register failed: ";
+
   private final NamingServerState state;
 
   public NamingServiceImpl(NamingServerState state) {
     this.state = state;
+  }
+
+  @Override
+  public void register(RegisterRequest request, StreamObserver<RegisterResponse> responseObserver) {
+    try {
+      this.state.registerServer(request.getService(), request.getQualifier(), request.getTarget());
+    } catch (RuntimeException e) {
+      Logger.debug(REGISTER_FAILED + e.getMessage());
+      responseObserver.onError(Status.UNKNOWN.withDescription(e.getMessage()).asRuntimeException());
+    }
   }
 }
