d5d7de3b8362e556b43a4f1aaaa15d403e001550
Rafael Oliveira <rafdev.x@gmail.com>
2023-03-17 15:04:35 +0000

    refactor(NamingServer): use ConcurrentMaps instead of Maps

--- NamingServer/src/main/java/pt/tecnico/distledger/namingserver/domain/NamingServerState.java
+++ NamingServer/src/main/java/pt/tecnico/distledger/namingserver/domain/NamingServerState.java
@@ -1,11 +1,11 @@
 package pt.tecnico.distledger.namingserver.domain;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /** Represents the current state of the naming server. */
 public class NamingServerState {
-  private final Map<String, ServiceEntry> services = new HashMap<>();
+  private final ConcurrentMap<String, ServiceEntry> services = new ConcurrentHashMap<>();
 
   public void registerServer(String service, String qualifier, String target) {
     this.services.computeIfAbsent(service, ServiceEntry::new).registerServer(qualifier, target);
