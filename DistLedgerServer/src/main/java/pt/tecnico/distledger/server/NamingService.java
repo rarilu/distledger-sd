fac3e7eb8ce8115e0429c24a3265d3f8da2c587e
Ricardo Antunes <me@riscadoa.com>
2023-03-20 10:53:36 +0000

    fix(Server): format code

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/NamingService.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/NamingService.java
@@ -2,10 +2,8 @@ package pt.tecnico.distledger.server;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import io.grpc.Status;
-import io.grpc.StatusRuntimeException;
-import pt.tecnico.distledger.contract.namingserver.NamingServiceGrpc;
 import pt.tecnico.distledger.contract.namingserver.NamingServerDistLedger.RegisterRequest;
+import pt.tecnico.distledger.contract.namingserver.NamingServiceGrpc;
 import pt.tecnico.distledger.utils.Logger;
 
 /**
@@ -13,43 +11,44 @@ import pt.tecnico.distledger.utils.Logger;
  * Naming Service.
  */
 public class NamingService implements AutoCloseable {
-    /** Target host and port for the well-known naming server. */
-    private static final String WELL_KNOWN_TARGET = "localhost:2002";
+  /** Target host and port for the well-known naming server. */
+  private static final String WELL_KNOWN_TARGET = "localhost:5001";
 
-    private final ManagedChannel channel;
-    private final NamingServiceGrpc.NamingServiceBlockingStub stub;
+  private final ManagedChannel channel;
+  private final NamingServiceGrpc.NamingServiceBlockingStub stub;
 
-    /** Creates a new NamingService, connecting to the well-known host and port. */
-    public NamingService() {
-        Logger.debug("Connecting to naming service at " + WELL_KNOWN_TARGET);
-        this.channel = ManagedChannelBuilder.forTarget(WELL_KNOWN_TARGET).usePlaintext().build();
-        this.stub = NamingServiceGrpc.newBlockingStub(this.channel);
-    }
+  /** Creates a new NamingService, connecting to the well-known host and port. */
+  public NamingService() {
+    Logger.debug("Connecting to naming service at " + WELL_KNOWN_TARGET);
+    this.channel = ManagedChannelBuilder.forTarget(WELL_KNOWN_TARGET).usePlaintext().build();
+    this.stub = NamingServiceGrpc.newBlockingStub(this.channel);
+  }
 
-    /** Executes a register request. */
-    public void register(String service, String qualifier, String target) {
-        RegisterRequest request = RegisterRequest.newBuilder()
+  /** Executes a register request. */
+  public void register(String service, String qualifier, String target) {
+    RegisterRequest request =
+        RegisterRequest.newBuilder()
             .setService(service)
             .setQualifier(qualifier)
             .setTarget(target)
             .build();
 
-        this.stub.register(request);
-    }
+    this.stub.register(request);
+  }
 
-    /** Executes a delete request. */
-    public void delete(String service, String target) {
-        // TODO
-    }
+  /** Executes a delete request. */
+  public void delete(String service, String target) {
+    // TODO
+  }
 
-    /** Executes a lookup request. */
-    public void lookup(String service, String qualifier) {
-        // TODO
-    }
+  /** Executes a lookup request. */
+  public void lookup(String service, String qualifier) {
+    // TODO
+  }
 
-    /** Close channel immediately. */
-    @Override
-    public void close() {
-        this.channel.shutdownNow();
-    }
+  /** Close channel immediately. */
+  @Override
+  public void close() {
+    this.channel.shutdownNow();
+  }
 }
