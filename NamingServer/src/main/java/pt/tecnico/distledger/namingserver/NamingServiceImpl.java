7244e3b6a79bfae6eb83b3cf9175a6f1998e6497
Ricardo Antunes <me@riscadoa.com>
2023-03-21 15:46:06 +0000

    feat(NamingServer): implement NamingServiceImpl.delete

--- NamingServer/src/main/java/pt/tecnico/distledger/namingserver/NamingServiceImpl.java
+++ NamingServer/src/main/java/pt/tecnico/distledger/namingserver/NamingServiceImpl.java
@@ -3,15 +3,19 @@ package pt.tecnico.distledger.namingserver;
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import pt.tecnico.distledger.common.Logger;
+import pt.tecnico.distledger.contract.namingserver.NamingServerDistLedger.DeleteRequest;
+import pt.tecnico.distledger.contract.namingserver.NamingServerDistLedger.DeleteResponse;
 import pt.tecnico.distledger.contract.namingserver.NamingServerDistLedger.RegisterRequest;
 import pt.tecnico.distledger.contract.namingserver.NamingServerDistLedger.RegisterResponse;
 import pt.tecnico.distledger.contract.namingserver.NamingServiceGrpc;
 import pt.tecnico.distledger.namingserver.domain.NamingServerState;
 import pt.tecnico.distledger.namingserver.domain.exceptions.DuplicateServerEntryException;
+import pt.tecnico.distledger.namingserver.domain.exceptions.ServerEntryNotFoundException;
 
 /** Implements the Admin service, handling gRPC requests. */
 public class NamingServiceImpl extends NamingServiceGrpc.NamingServiceImplBase {
   private static final String REGISTER_FAILED = "Register failed: ";
+  private static final String DELETE_FAILED = "Delete failed: ";
 
   private final NamingServerState state;
 
@@ -31,6 +35,22 @@ public class NamingServiceImpl extends NamingServiceGrpc.NamingServiceImplBase {
           Status.ALREADY_EXISTS.withDescription(e.getMessage()).asRuntimeException());
     } catch (RuntimeException e) {
       Logger.debug(REGISTER_FAILED + e.getMessage());
+      responseObserver.onError(Status.UNKNOWN.withDescription(e.getMessage()).asRuntimeException());
+    }
+  }
+
+  @Override
+  public void delete(DeleteRequest request, StreamObserver<DeleteResponse> responseObserver) {
+    try {
+      this.state.deleteServer(request.getService(), request.getTarget());
+      responseObserver.onNext(DeleteResponse.getDefaultInstance());
+      responseObserver.onCompleted();
+    } catch (ServerEntryNotFoundException e) {
+      Logger.debug(DELETE_FAILED + e.getMessage());
+      responseObserver.onError(
+          Status.NOT_FOUND.withDescription(e.getMessage()).asRuntimeException());
+    } catch (RuntimeException e) {
+      Logger.debug(DELETE_FAILED + e.getMessage());
       responseObserver.onError(Status.UNKNOWN.withDescription(e.getMessage()).asRuntimeException());
     }
   }
