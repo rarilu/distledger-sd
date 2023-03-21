f89cc75f6cfd2e1d07bbde873bbe84414913bc68
Ricardo Antunes <me@riscadoa.com>
2023-03-21 17:16:28 +0000

    docs(NamingServer): add missing javadocs

--- NamingServer/src/main/java/pt/tecnico/distledger/namingserver/domain/NamingServerState.java
+++ NamingServer/src/main/java/pt/tecnico/distledger/namingserver/domain/NamingServerState.java
@@ -7,10 +7,12 @@ import java.util.concurrent.ConcurrentMap;
 public class NamingServerState {
   private final ConcurrentMap<String, ServiceEntry> services = new ConcurrentHashMap<>();
 
+  /** Registers a new server entry for the given service and target. */
   public void registerServer(String service, String qualifier, String target) {
     this.services.computeIfAbsent(service, ServiceEntry::new).registerServer(qualifier, target);
   }
 
+  /** Deletes the server entry for the given service and target. */
   public void deleteServer(String service, String target) {
     this.services.computeIfPresent(
         service,
