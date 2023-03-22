2d67d33ed5e386ab72ceaaf020cc02f3bfc44e74
Ricardo Antunes <me@riscadoa.com>
2023-03-22 14:56:10 +0000

    test(Server): cover NamingServiceImpl runtime exceptions

--- NamingServer/src/test/groovy/pt/tecnico/distledger/namingserver/NamingServiceImplTest.groovy
+++ NamingServer/src/test/groovy/pt/tecnico/distledger/namingserver/NamingServiceImplTest.groovy
@@ -42,4 +42,31 @@ class NamingServiceImplTest extends Specification {
         and: "response is sent"
         1 * observer.onNext(ShutdownResponse.getDefaultInstance())
     }
+
+    def "catch runtime exceptions"() {
+        given: "a mock state that throws an exception when a method is called"
+        def state = Mock(NamingServerState)
+        state.registerServer(_) >> { throw new RuntimeException("Unknown error") }
+        state.deleteServer(_) >> { throw new RuntimeException("Unknown error") }
+
+        and: "an observer that throws an exception when onNext is called"
+        observer.onNext(_) >> { throw new RuntimeException("Unknown error") }
+
+        and: "a service that uses the mock state"
+        service = new NamingServiceImpl(state)
+
+        and: "a mock server which is passed to the service"
+        service.setServer(Mock(io.grpc.Server))
+
+        when: "a method is called"
+        method.invoke(service, method.getParameterTypes()[0].getDefaultInstance(), observer)
+
+        then: "method fails with RuntimeException"
+        1 * observer.onError({
+            it instanceof StatusRuntimeException && it.getMessage() == "UNKNOWN: Unknown error"
+        })
+
+        where: "method is any void function of NamingServiceImpl that takes 2 arguments"
+        method << NamingServiceImpl.class.getDeclaredMethods().findAll { it.getReturnType() == void.class && it.getParameterTypes().size() == 2 }
+    }
 }
