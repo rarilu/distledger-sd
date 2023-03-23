c891d4fc506bd522832d0c138d24e395fbf4e45d
Luís Fonseca <luis@lhf.pt>
2023-03-23 19:52:16 +0000

    test(NamingServer): test NamingServerState

--- NamingServer/src/test/groovy/pt/tecnico/distledger/namingserver/domain/NamingServerStateTest.groovy
+++ NamingServer/src/test/groovy/pt/tecnico/distledger/namingserver/domain/NamingServerStateTest.groovy
@@ -1,6 +1,7 @@
 package pt.tecnico.distledger.namingserver.domain
 
 import pt.tecnico.distledger.namingserver.domain.exceptions.DuplicateServerEntryException
+import pt.tecnico.distledger.namingserver.domain.exceptions.ServerEntryNotFoundException
 import spock.lang.Specification
 
 class NamingServerStateTest extends Specification {
@@ -19,5 +20,53 @@ class NamingServerStateTest extends Specification {
 
         then: "an exception is thrown"
         thrown(DuplicateServerEntryException)
+    }
+
+    def "register a server"() {
+        when: "a server is registered"
+        namingServerState.registerServer("DistLedger", "A", "localhost:8000")
+
+        then: "the lookup returns the server"
+        namingServerState.lookupServer("DistLedger", "A") == ["localhost:8000"]
+    }
+
+    def "delete a server"() {
+        given: "a server already registered"
+        namingServerState.registerServer("DistLedger", "A", "localhost:8000")
+
+        when: "the server is deleted"
+        namingServerState.deleteServer("DistLedger", "localhost:8000")
+
+        then: "the lookup returns no servers"
+        namingServerState.lookupServer("DistLedger", "A").isEmpty()
+    }
+
+    def "delete a server with a non-existant service"() {
+        when: "a non-existant server is deleted"
+        namingServerState.deleteServer("DistLedger", "localhost:8000")
+
+        then: "an exception is thrown"
+        thrown(ServerEntryNotFoundException)
+    }
+
+    def "delete a non-existant server"() {
+        given: "a server already registered"
+        namingServerState.registerServer("DistLedger", "A", "localhost:8000")
+
+        when: "a non-existant server with the same service is deleted"
+        namingServerState.deleteServer("DistLedger", "localhost:8001")
+
+        then: "an exception is thrown"
+        thrown(ServerEntryNotFoundException)
+    }
+
+    def "lookup multiple servers"() {
+        when: "multiple servers are registered"
+        namingServerState.registerServer("DistLedger", "A", "localhost:8000")
+        namingServerState.registerServer("DistLedger", "A", "localhost:8001")
+        namingServerState.registerServer("DistLedger", "A", "localhost:8002")
+
+        then: "the lookup returns the servers"
+        namingServerState.lookupServer("DistLedger", "A") == ["localhost:8000", "localhost:8001", "localhost:8002"]
     }
 }
