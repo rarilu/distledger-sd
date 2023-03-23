c1bdcca60772a235fd8e9e747388fabcd60ddb8c
Luís Fonseca <luis@lhf.pt>
2023-03-23 14:45:54 +0000

    refactor(Server): propagate cause of FailedPropagationException

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/exceptions/FailedPropagationException.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/exceptions/FailedPropagationException.java
@@ -1,8 +1,8 @@
 package pt.tecnico.distledger.server.domain.exceptions;
 
-/** Represents an exception thrown when an attempt to propagate state fails */
+/** Represents an exception thrown when an attempt to propagate state fails. */
 public class FailedPropagationException extends RuntimeException {
-  public FailedPropagationException() {
-    super("Failed to propagate state to server");
+  public FailedPropagationException(Exception cause) {
+    super(cause);
   }
 }
