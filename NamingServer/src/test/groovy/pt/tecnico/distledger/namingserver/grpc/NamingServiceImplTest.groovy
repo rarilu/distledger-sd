56619360a7d7e724a8f8b6450c4c14e5cc6035dd
Luís Fonseca <luis@lhf.pt>
2023-03-24 16:17:57 +0000

    test(Server): cover NamingServiceImpl

--- NamingServer/src/test/groovy/pt/tecnico/distledger/namingserver/grpc/NamingServiceImplTest.groovy
+++ NamingServer/src/test/groovy/pt/tecnico/distledger/namingserver/grpc/NamingServiceImplTest.groovy
@@ -5,15 +5,98 @@ import spock.lang.Specification
 import io.grpc.StatusRuntimeException
 import io.grpc.stub.StreamObserver
 import pt.tecnico.distledger.namingserver.domain.NamingServerState
+import pt.tecnico.distledger.contract.namingserver.NamingServerDistLedger.DeleteRequest
+import pt.tecnico.distledger.contract.namingserver.NamingServerDistLedger.DeleteResponse
+import pt.tecnico.distledger.contract.namingserver.NamingServerDistLedger.LookupRequest
+import pt.tecnico.distledger.contract.namingserver.NamingServerDistLedger.LookupResponse
+import pt.tecnico.distledger.contract.namingserver.NamingServerDistLedger.RegisterRequest
+import pt.tecnico.distledger.contract.namingserver.NamingServerDistLedger.RegisterResponse
 
 class NamingServiceImplTest extends Specification {
+    def state
     def service
     def observer
 
     def setup() {
-        def state = new NamingServerState()
+        state = new NamingServerState()
         service = new NamingServiceImpl(state)
         observer = Mock(StreamObserver)
+    }
+
+    def "register a server"() {
+        when: "a server is registered"
+        service.register(RegisterRequest.newBuilder().setService("DistLedger").setQualifier("A").setTarget("localhost:2000").build(), observer)
+
+        then: "the correct response is received"
+        1 * observer.onNext(RegisterResponse.getDefaultInstance())
+    }
+
+    def "register an already registered server"() {
+        given: "a server already registered"
+        state.registerServer("DistLedger", "A", "localhost:2000")
+
+        when: "the server is registered again"
+        service.register(RegisterRequest.newBuilder().setService("DistLedger").setQualifier("A").setTarget("localhost:2000").build(), observer)
+
+        then: "an exception is thrown"
+        1 * observer.onError({
+            it instanceof StatusRuntimeException && it.getMessage() == "ALREADY_EXISTS: An entry for server with target localhost:2000 and service DistLedger already exists"
+        })
+    }
+
+    def "delete a server"() {
+        given: "a server registered"
+        state.registerServer("DistLedger", "A", "localhost:2000")
+
+        when: "the server is deleted"
+        service.delete(DeleteRequest.newBuilder().setService("DistLedger").setTarget("localhost:2000").build(), observer)
+
+        then: "the correct response is received"
+        1 * observer.onNext(DeleteResponse.getDefaultInstance())
+    }
+
+    def "delete a non-existent server"() {
+        when: "a server is deleted"
+        service.delete(DeleteRequest.newBuilder().setService("DistLedger").setTarget("localhost:2000").build(), observer)
+
+        then: "an exception is thrown"
+        1 * observer.onError({
+            it instanceof StatusRuntimeException && it.getMessage() == "NOT_FOUND: An entry for server with target localhost:2000 and service DistLedger was not found"
+        })
+    }
+
+    def "lookup a server"() {
+        given: "a server registered"
+        state.registerServer("DistLedger", "A", "localhost:2000")
+
+        when: "the server is looked up"
+        service.lookup(LookupRequest.newBuilder().setService(serviceName).setQualifier(qualifier).build(), observer)
+
+        then: "the correct response is received"
+        1 * observer.onNext({
+            it instanceof LookupResponse && it.getTargetsList() == targetsList
+        })
+
+        where:
+        serviceName  | qualifier | targetsList
+        "DistLedger" | "A"       | ["localhost:2000"]
+        "DistLedger" | "void"    | []
+        "void"       | "A"       | []
+    }
+
+    def "lookup all servers in a service"() {
+        given: "servers registered"
+        state.registerServer("DistLedger", "A", "localhost:2000")
+        state.registerServer("DistLedger", "B", "localhost:2001")
+        state.registerServer("DistLedger", "C", "localhost:2002")
+
+        when: "the servers are looked up"
+        service.lookup(LookupRequest.newBuilder().setService("DistLedger").build(), observer)
+
+        then: "the correct response is received"
+        1 * observer.onNext({
+            it instanceof LookupResponse && it.getTargetsList() == ["localhost:2000", "localhost:2001", "localhost:2002"]
+        })
     }
 
     def "catch runtime exceptions"() {
