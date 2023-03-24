b0dfc63694ce04b3b61a40bc8607b1ca92726858
Ricardo Antunes <me@riscadoa.com>
2023-03-24 10:41:24 +0000

    feat(Server): use naming service in CrossServerService

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/exceptions/FailedPropagationException.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/exceptions/FailedPropagationException.java
@@ -2,6 +2,10 @@ package pt.tecnico.distledger.server.domain.exceptions;
 
 /** Represents an exception thrown when an attempt to propagate state fails. */
 public class FailedPropagationException extends RuntimeException {
+  public FailedPropagationException(String message) {
+    super(message);
+  }
+
   public FailedPropagationException(Exception cause) {
     super(cause);
   }
--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/grpc/CrossServerService.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/grpc/CrossServerService.java
@@ -1,26 +1,21 @@
 package pt.tecnico.distledger.server.grpc;
 
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import io.grpc.StatusRuntimeException;
 import pt.tecnico.distledger.common.Logger;
+import pt.tecnico.distledger.common.grpc.BaseService;
+import pt.tecnico.distledger.common.grpc.NamingService;
 import pt.tecnico.distledger.contract.distledgerserver.CrossServerDistLedger.PropagateStateRequest;
 import pt.tecnico.distledger.contract.distledgerserver.DistLedgerCrossServerServiceGrpc;
 import pt.tecnico.distledger.server.domain.exceptions.FailedPropagationException;
 import pt.tecnico.distledger.server.visitors.LedgerStateGenerator;
 
 /** Handles CrossServer operations, making gRPC requests to the server's CrossServer service. */
-public class CrossServerService implements AutoCloseable {
-  private final ManagedChannel channel;
-  private final DistLedgerCrossServerServiceGrpc.DistLedgerCrossServerServiceBlockingStub stub;
+public class CrossServerService
+    extends BaseService<DistLedgerCrossServerServiceGrpc.DistLedgerCrossServerServiceBlockingStub> {
+  private static final int MAX_TRIES = 2;
 
-  /** Creates a new CrossServerService, connecting to the given host and port. */
-  public CrossServerService(String host, int port) {
-    final String target = host + ":" + port;
-    Logger.debug("Connecting to " + target);
-
-    this.channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
-    this.stub = DistLedgerCrossServerServiceGrpc.newBlockingStub(this.channel);
+  /** Creates a new CrossServerService using the given NamingService. */
+  public CrossServerService(NamingService service) {
+    super(service, DistLedgerCrossServerServiceGrpc::newBlockingStub);
   }
 
   /**
@@ -34,15 +29,16 @@ public class CrossServerService implements AutoCloseable {
         PropagateStateRequest.newBuilder().setState(generator.build()).build();
     try {
       Logger.debug("Sending request: " + request.toString());
-      this.stub.propagateState(request);
-    } catch (StatusRuntimeException e) {
+
+      this.makeRequestWithRetryInvalidatingStubCache(
+              server,
+              request,
+              DistLedgerCrossServerServiceGrpc.DistLedgerCrossServerServiceBlockingStub
+                  ::propagateState,
+              MAX_TRIES)
+          .orElseThrow(() -> new FailedPropagationException("Secondary server is unavailable"));
+    } catch (RuntimeException e) {
       throw new FailedPropagationException(e);
     }
-  }
-
-  /** Close channel immediately. */
-  @Override
-  public void close() {
-    this.channel.shutdownNow();
   }
 }
