7db97007a2c08b8c4b031b68145c7f5ac3604ca9
Rafael Oliveira <rafdev.x@gmail.com>
2023-03-24 17:25:06 +0000

    docs(Common): add missing JavaDoc comments

--- Common/src/main/java/pt/tecnico/distledger/common/grpc/exceptions/DuplicateQualifierException.java
+++ Common/src/main/java/pt/tecnico/distledger/common/grpc/exceptions/DuplicateQualifierException.java
@@ -1,7 +1,14 @@
 package pt.tecnico.distledger.common.grpc.exceptions;
 
-/** Represents an exception thrown when there is more than one server with the same qualifier. */
+/**
+ * Represents an exception thrown when there is more than one server with the same qualifier.
+ *
+ * <p>The current consistency model and base assumptions used in the DistLedger do not allow for
+ * multiple servers to have the same qualifier. This exception is thrown when this is detected,
+ * alerting for possibly inconsistent state.
+ */
 public class DuplicateQualifierException extends RuntimeException {
+  /** Creates a new DuplicateQualifierException given the qualifier that multiple servers have. */
   public DuplicateQualifierException(String qualifier) {
     super(
         "More than one server found for the qualifier "
