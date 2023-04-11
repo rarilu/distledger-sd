3cc000678fbccf586cd03be73ef2253a8af9b061
Rafael Oliveira <rafdev.x@gmail.com>
2023-04-11 23:01:40 +0100

    feat(Common): add utility class for proto objects manipulation

---
+++ Common/src/main/java/pt/tecnico/distledger/common/grpc/ProtoUtils.java
@@ -0,0 +1,17 @@
+package pt.tecnico.distledger.common.grpc;
+
+import java.util.Arrays;
+import pt.tecnico.distledger.common.domain.VectorClock;
+import pt.tecnico.distledger.contract.DistLedgerCommonDefinitions;
+
+/** Collection of utility methods for gRPC Proto objects manipulation. */
+public class ProtoUtils {
+  private ProtoUtils() {}
+
+  /** Convert a VectorClock domain entity to a Proto object. */
+  public static DistLedgerCommonDefinitions.VectorClock toProto(VectorClock vectorClock) {
+    return DistLedgerCommonDefinitions.VectorClock.newBuilder()
+        .addAllValues(Arrays.stream(vectorClock.toArray()).boxed().toList())
+        .build();
+  }
+}
