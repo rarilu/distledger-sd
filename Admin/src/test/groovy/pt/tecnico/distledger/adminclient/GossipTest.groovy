0719dd53cbe643a0f379bb421dfad3883c41ebd6
Ricardo Antunes <me@riscadoa.com>
2023-04-12 11:25:39 +0100

    test(Admin): update gossip test

--- Admin/src/test/groovy/pt/tecnico/distledger/adminclient/GossipTest.groovy
+++ Admin/src/test/groovy/pt/tecnico/distledger/adminclient/GossipTest.groovy
@@ -1,15 +1,73 @@
 package pt.tecnico.distledger.adminclient
 
+import io.grpc.Status
+import pt.tecnico.distledger.contract.admin.AdminServiceGrpc
+import pt.tecnico.distledger.contract.admin.AdminDistLedger.GossipRequest
+import pt.tecnico.distledger.contract.admin.AdminDistLedger.GossipResponse
+import org.grpcmock.GrpcMock
+
 class GossipTest extends BaseTest {
-    // TODO: update tests when gossip is implemented
-    def "gossip outputs a todo message"() {
-        given: "a gossip input"
-        provideInput("gossip\nexit\n")
+    def "user provides invalid gossip command"() {
+        given: "an invalid gossip input"
+        provideInput("gossip wrong usage\nexit\n")
 
         when: "the admin client is run"
         runMain()
 
         then: "the output is correct"
-        outBuf.toString() == "> TODO: implement gossip command (only for Phase-3)\n> "
+        outBuf.toString() == ("> " + EXPECTED_USAGE_STRING + "\n> ")
+
+        and: "the mock server received no requests"
+        GrpcMock.verifyThat(GrpcMock.calledMethod(AdminServiceGrpc.getGossipMethod()), GrpcMock.never())
+    }
+
+    def "gossip server returns empty response"() {
+        given: "a gossip input"
+        provideInput("gossip A\nexit\n")
+
+        and: "a mock server that returns an empty response"
+        GrpcMock.stubFor(
+                GrpcMock.unaryMethod(AdminServiceGrpc.getGossipMethod())
+                        .willReturn(GrpcMock.response(GossipResponse.getDefaultInstance())))
+
+        when: "the admin client is run"
+        runMain()
+
+        then: "the output is correct"
+        outBuf.toString() == "> OK\n\n> "
+
+        and: "the mock server received the correct request, exactly once"
+        GrpcMock.verifyThat(
+                GrpcMock.calledMethod(AdminServiceGrpc.getGossipMethod())
+                        .withRequest(GossipRequest.getDefaultInstance()),
+                GrpcMock.times(1)
+        )
+    }
+
+    def "gossip returns a status exception"() {
+        given: "a gossip server input"
+        provideInput("gossip A\nexit\n")
+
+        and: "a mock server that returns a status exception"
+        GrpcMock.stubFor(
+                GrpcMock.unaryMethod(AdminServiceGrpc.getGossipMethod())
+                        .willReturn(GrpcMock.statusException(
+                                Status.UNKNOWN
+                                        .withDescription("The server raised an exception")
+                        ))
+        )
+
+        when: "the admin client is run"
+        runMain()
+
+        then: "the output is correct"
+        outBuf.toString() == "> Error: UNKNOWN: The server raised an exception\n\n> "
+
+        and: "the mock server received the correct request, exactly once"
+        GrpcMock.verifyThat(
+                GrpcMock.calledMethod(AdminServiceGrpc.getGossipMethod())
+                        .withRequest(GossipRequest.getDefaultInstance()),
+                GrpcMock.times(1)
+        )
     }
 }
