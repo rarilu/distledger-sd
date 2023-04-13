37ad645dc64b5b9cf0780f92c174b6aaf79c9103
Ricardo Antunes <me@riscadoa.com>
2023-04-13 19:44:47 +0100

    feat(Server): implement gossip update log in ServerState

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/ServerState.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/domain/ServerState.java
@@ -6,10 +6,13 @@ import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import pt.tecnico.distledger.common.domain.VectorClock;
+import pt.tecnico.distledger.common.domain.VectorClock.Order;
 import pt.tecnico.distledger.server.domain.exceptions.OutdatedStateException;
 import pt.tecnico.distledger.server.domain.exceptions.UnknownAccountException;
 import pt.tecnico.distledger.server.domain.operation.Operation;
+import pt.tecnico.distledger.server.visitors.OperationExecutor;
 import pt.tecnico.distledger.server.visitors.OperationVisitor;
 
 /** Represents the current state of the server. */
@@ -18,6 +21,9 @@ public class ServerState {
   private final List<Operation> ledger = Collections.synchronizedList(new ArrayList<>());
   private final ConcurrentMap<String, Account> accounts = new ConcurrentHashMap<>();
   private final VectorClock valueTimeStamp = new VectorClock();
+  private final VectorClock replicaTimeStamp = new VectorClock();
+  private AtomicInteger firstUnstable = new AtomicInteger(0);
+  private OperationExecutor executor = new OperationExecutor(this);
 
   public ServerState(int id) {
     this.id = id;
@@ -25,26 +31,113 @@ public class ServerState {
   }
 
   /**
-   * Register a given operation in the ledger.
+   * Generates a new unique timestamp for an operation received in this replica from a client.
+   * Increments the timestamp of this replica and merges it with the timestamp of the client.
    *
-   * @return the prev timestamp of the operation, merged with the current value timestamp at this
-   *     replica's slot.
+   * @param prevTimeStamp the timestamp of the client when it sent the operation
+   * @return the new timestamp.
    */
-  public VectorClock addToLedger(Operation op) {
+  public VectorClock generateTimeStamp(VectorClock prevTimeStamp) {
     VectorClock timeStamp;
 
-    // Safety: it's okay do this without locking the ledger since the order in which the clock is
-    // merged doesn't matter: the end result is the same
-    synchronized (this.valueTimeStamp) {
-      this.valueTimeStamp.merge(op.getPrevTimeStamp());
-      timeStamp = new VectorClock(this.valueTimeStamp);
-      timeStamp.mergeSingle(op.getPrevTimeStamp(), this.id);
+    // Safety: the timestamp is locked to avoid concurrent increments
+    synchronized (this.replicaTimeStamp) {
+      this.replicaTimeStamp.increment(this.id);
+      timeStamp = new VectorClock(this.replicaTimeStamp);
+      timeStamp.mergeSingle(this.replicaTimeStamp, this.id);
     }
 
-    // Safety: synchronized list, it's okay to add to it without a synchronized
-    // block
-    this.ledger.add(op);
     return timeStamp;
+  }
+
+  /**
+   * Register a given operation in the ledger. If the operation can be executed immediately, it will
+   * immediatelly be stabilized. Otherwise, it will be added to the ledger and stabilized later.
+   *
+   * @param rejectDuplicates if true, will reject if TS <= replicaTS
+   * @return true if it was immediately stabilized, false otherwise.
+   */
+  public boolean addToLedger(Operation op, boolean rejectDuplicates) {
+    // Check if the operation is a duplicate
+    if (rejectDuplicates && this.isDuplicate(op)) {
+      return false;
+    }
+
+    // Merge the replica timestamp with the operation timestamp
+    synchronized (this.replicaTimeStamp) {
+      this.replicaTimeStamp.merge(op.getTimeStamp());
+    }
+
+    // Check if the operation can be immediately executed
+    if (this.canStabilize(op)) {
+      // If it can, mark it as stable and execute it
+      op.setStable();
+      this.execute(op);
+
+      // Add it to the ledger, before the first unstable operation
+      int index = this.firstUnstable.getAndIncrement();
+
+      // Safety: the ledger must be locked to avoid concurrent modifications
+      synchronized (this.ledger) {
+        if (index < this.ledger.size()) {
+          // Swap the operation with the first unstable operation, if it's not already there
+          this.ledger.add(this.ledger.get(index));
+          this.ledger.set(index, op);
+        } else {
+          this.ledger.add(op);
+        }
+      }
+
+      return true;
+    } else {
+      // If it can't, just add it to the ledger and wait for stabilization
+      // Safety: synchronized list, it's okay to add to it without a synchronized block.
+      this.ledger.add(op);
+      return false;
+    }
+  }
+
+  /** Executes all operations in the ledger that can be executed. */
+  public void stabilize() {
+    boolean isStable;
+
+    do {
+      isStable = true;
+
+      for (int i = this.firstUnstable.get(), last = this.ledger.size(); i < last; i++) {
+        Operation op = this.ledger.get(i);
+
+        // Safety: we first sync on the operation, and check if it's stable. If it was already
+        // marked
+        // as stable, we skip it since it means another thread is already executing it. If it's not
+        // stable, we can set it as stable and leave the synchronized block, since other threads
+        // will
+        // see it as stable and skip it.
+        synchronized (op) {
+          // If the operation is already stable or can't be stabilized, skip it
+          if (op.isStable() || !this.canStabilize(op)) {
+            continue;
+          }
+
+          op.setStable();
+        }
+
+        // Swap the operation with the first unstable operation, if it's not already there
+        int swapIndex = firstUnstable.getAndIncrement();
+        if (swapIndex != i) {
+          synchronized (this.ledger) {
+            Operation swap = this.ledger.get(swapIndex);
+            this.ledger.set(swapIndex, op);
+            this.ledger.set(i, swap);
+          }
+        }
+
+        this.execute(op);
+
+        // Unset the isStable flag, so that stabilize searches for new operations to stabilize
+        isStable = false;
+      }
+    } while (!isStable);
   }
 
   /**
@@ -57,7 +150,8 @@ public class ServerState {
   public Optional<Integer> visitLedger(OperationVisitor visitor, int startAtIndex) {
     // Safety: prevent operations from being added to the ledger while we are
     // visiting it
-    // Operations themselves are read-only, so that's not an issue
+    // Operations themselves are thread-safe, so we don't need to lock them: the only mutable
+    // operation state is atomic.
     synchronized (this.ledger) {
       if (this.ledger.isEmpty()) {
         return Optional.empty();
@@ -110,5 +204,42 @@ public class ServerState {
   /** Returns the ID of the server. */
   public int getId() {
     return this.id;
+  }
+
+  /** Executes an operation. */
+  private void execute(Operation op) {
+    // Execute the operation, possibly concurrently with other threads
+    try {
+      op.accept(this.executor);
+
+      // Merge the operation's timestamp with the current value timestamp
+      synchronized (this.valueTimeStamp) {
+        this.valueTimeStamp.merge(op.getTimeStamp());
+      }
+    } catch (RuntimeException e) {
+      // If the operation fails, mark it as failed and log the error
+      op.setFailed();
+      System.err.println("Operation failed: " + e.getMessage());
+    }
+  }
+
+  /** Checks if an operation can be stabilized. */
+  private boolean canStabilize(Operation op) {
+    VectorClock.Order order;
+    synchronized (this.valueTimeStamp) {
+      order = VectorClock.compare(op.getPrevTimeStamp(), this.valueTimeStamp);
+    }
+
+    return order == Order.BEFORE || order == Order.EQUAL;
+  }
+
+  /** Checks if an operation is a duplicate. */
+  private boolean isDuplicate(Operation op) {
+    VectorClock.Order order;
+    synchronized (this.replicaTimeStamp) {
+      order = VectorClock.compare(op.getTimeStamp(), this.replicaTimeStamp);
+    }
+
+    return order == Order.BEFORE || order == Order.EQUAL;
   }
 }
