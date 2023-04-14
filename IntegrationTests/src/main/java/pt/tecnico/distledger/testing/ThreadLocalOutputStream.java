7a64d7f99f1e7c49ad7eaaf09c8dd50058749f3a
Ricardo Antunes <me@riscadoa.com>
2023-04-14 14:07:44 +0100

    test(IntegrationTests): allow multiple servers and client state in BaseIT

---
+++ IntegrationTests/src/main/java/pt/tecnico/distledger/testing/ThreadLocalOutputStream.java
@@ -0,0 +1,32 @@
+package pt.tecnico.distledger.testing;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * OutputStream that delegates to a ThreadLocal. Useful in the integration tests, where we need to
+ * provide a different OutputStream to each thread.
+ */
+class ThreadLocalOutputStream extends OutputStream {
+  private final ThreadLocal<OutputStream> stream;
+
+  /** Creates a new ThreadLocalOutputStream. */
+  public ThreadLocalOutputStream(ThreadLocal<OutputStream> stream) {
+    this.stream = stream;
+  }
+
+  @Override
+  public void write(byte[] b, int off, int len) throws IOException {
+    stream.get().write(b, off, len);
+  }
+
+  @Override
+  public void write(int b) throws IOException {
+    stream.get().write(b);
+  }
+
+  /** Sets the OutputStream to delegate to in the current thread. */
+  public void setStream(OutputStream stream) {
+    this.stream.set(stream);
+  }
+}
