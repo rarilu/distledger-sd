0c72799752dc6f1c05c115e88c59cec7b8ef6d06
Rafael Oliveira <rafdev.x@gmail.com>
2023-04-14 20:31:47 +0100

    feat(NamingServer): prevent servers with same service and qualifier
    Closes #222

--- NamingServer/src/main/java/pt/tecnico/distledger/namingserver/domain/exceptions/DuplicateServerEntryException.java
+++ NamingServer/src/main/java/pt/tecnico/distledger/namingserver/domain/exceptions/DuplicateServerEntryException.java
@@ -7,12 +7,14 @@ public class DuplicateServerEntryException extends RuntimeException {
    * Creates a new DuplicateServerEntryException given details of the server entry whose creation
    * was attempted.
    */
-  public DuplicateServerEntryException(String service, String target) {
+  public DuplicateServerEntryException(String service, String qualifier, String target) {
     super(
-        "An entry for server with target "
-            + target
-            + " and service "
+        "An entry in service "
             + service
+            + " for target "
+            + target
+            + " or qualifier "
+            + qualifier
             + " already exists");
   }
 }
