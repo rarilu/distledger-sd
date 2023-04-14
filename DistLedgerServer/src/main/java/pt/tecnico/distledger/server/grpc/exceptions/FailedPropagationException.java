e17c6fc434c342cfc5c87bf3e694ca49d0c4b208
Rafael Oliveira <rafdev.x@gmail.com>
2023-04-14 23:33:35 +0100

    style(Server): remove unused FailedPropagationException constructor

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/grpc/exceptions/FailedPropagationException.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/grpc/exceptions/FailedPropagationException.java
@@ -2,10 +2,6 @@ package pt.tecnico.distledger.server.grpc.exceptions;
 
 /** Represents an exception thrown when an attempt to propagate state fails. */
 public class FailedPropagationException extends RuntimeException {
-  public FailedPropagationException(String message) {
-    super(message);
-  }
-
   public FailedPropagationException(Exception cause) {
     super(cause.getMessage(), cause);
   }
