b53d9c4511e349900f0d8bfda06e48dbb31875bf
Rafael Oliveira <rafdev.x@gmail.com>
2023-04-14 21:03:51 +0100

    refactor(NamingServer): stop using ServerEntry lists in ServiceEntry
    Since we no longer allow multiple servers with the same qualifier in the
    same service, a list is no longer necessary.

--- NamingServer/src/main/java/pt/tecnico/distledger/namingserver/domain/ServiceEntry.java
+++ NamingServer/src/main/java/pt/tecnico/distledger/namingserver/domain/ServiceEntry.java
@@ -1,8 +1,7 @@
 package pt.tecnico.distledger.namingserver.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -13,7 +12,7 @@ import pt.tecnico.distledger.namingserver.domain.exceptions.ServerEntryNotFoundE
 /** Represents a service entry in the naming server. */
 public class ServiceEntry {
   private final String name;
-  private final ConcurrentMap<String, List<ServerEntry>> servers = new ConcurrentHashMap<>();
+  private final ConcurrentMap<String, ServerEntry> servers = new ConcurrentHashMap<>();
   private final Set<String> targets = ConcurrentHashMap.newKeySet();
   private final AtomicInteger nextId = new AtomicInteger(0);
 
@@ -29,40 +28,24 @@ public class ServiceEntry {
   public int registerServer(String qualifier, String target) {
     // Add the target to the set of registered targets, and throw an exception if it was already
     // present
-    if (!this.targets.add(target) || this.servers.containsKey(qualifier)) {
-      this.targets.remove(target); // remove target if it was added
+    if (!this.targets.add(target)) {
       throw new DuplicateServerEntryException(this.name, qualifier, target);
     }
 
     int id = this.nextId.getAndIncrement();
-
-    // Add the server entry to the list of servers for the given qualifier
-    this.servers.compute(
-        qualifier,
-        (k, serverEntries) -> {
-          ServerEntry newEntry = new ServerEntry(k, target, id);
-
-          if (serverEntries == null) {
-            serverEntries = Collections.synchronizedList(new ArrayList<>(List.of(newEntry)));
-          } else {
-            serverEntries.add(newEntry);
-          }
-
-          return serverEntries;
-        });
+    if (this.servers.putIfAbsent(qualifier, new ServerEntry(qualifier, target, id)) != null) {
+      // an entry is already present, do not override it
+      throw new DuplicateServerEntryException(this.name, qualifier, target);
+    }
 
     return id;
   }
 
   /** Deletes a server from the service entry. */
   public void deleteServer(String target) {
-    // Safety: no need to synchronize while iterating the concurrent map; standard says it's safe
-    for (List<ServerEntry> serverEntries : this.servers.values()) {
-      if (serverEntries.removeIf(serverEntry -> target.equals(serverEntry.target()))) {
-        this.targets.remove(target); // remove the target from the set of registered targets
-        this.servers.values().removeIf(List::isEmpty);
-        return;
-      }
+    if (this.servers.values().removeIf(serverEntry -> target.equals(serverEntry.target()))) {
+      this.targets.remove(target); // remove the target from the set of registered targets
+      return;
     }
 
     // If we reach this point, the server was not found
@@ -70,29 +53,12 @@ public class ServiceEntry {
   }
 
   /** Looks up servers with the given qualifier in the service entry. */
-  public List<ServerEntry> lookup(String qualifier) {
-    List<ServerEntry> serverEntries = this.servers.getOrDefault(qualifier, List.of());
-
-    // Safety: we need to synchronize on the server entries list since we are iterating over it
-    // It's okay to return the records without cloning them, since they are immutable
-    synchronized (serverEntries) {
-      return serverEntries.stream().filter(entry -> qualifier.equals(entry.qualifier())).toList();
-    }
+  public Optional<ServerEntry> lookup(String qualifier) {
+    return Optional.ofNullable(this.servers.get(qualifier));
   }
 
   /** Looks up all servers in the service entry. */
   public List<ServerEntry> lookup() {
-    List<ServerEntry> entries = new ArrayList<>();
-
-    // Safety: no need to synchronize while iterating the concurrent map; standard says it's safe
-    // It's okay to return the records without cloning them, since they are immutable
-    for (List<ServerEntry> serverEntries : this.servers.values()) {
-      // Safety: we need to synchronize on the server entries list since we are iterating over it
-      synchronized (serverEntries) {
-        entries.addAll(serverEntries);
-      }
-    }
-
-    return entries;
+    return this.servers.values().stream().toList();
   }
 }
