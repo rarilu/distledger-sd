59ba20b1329c4f078f1838b3756cd000051767a5
Ricardo Antunes <me@riscadoa.com>
2023-04-14 22:24:50 +0100

    test(IntegrationTests): check if failures are propagated correctly

--- DistLedgerServer/src/main/java/pt/tecnico/distledger/server/grpc/DistLedgerCrossServerServiceImpl.java
+++ DistLedgerServer/src/main/java/pt/tecnico/distledger/server/grpc/DistLedgerCrossServerServiceImpl.java
@@ -41,22 +41,25 @@ public class DistLedgerCrossServerServiceImpl
   }
 
   private Operation parseOperation(DistLedgerCommonDefinitions.Operation operation) {
-    Operation op =
-        switch (operation.getType()) {
-          case OP_CREATE_ACCOUNT -> new CreateOp(
+    Operation op;
+
+    switch (operation.getType()) {
+      case OP_CREATE_ACCOUNT -> op =
+          new CreateOp(
               operation.getUserId(),
               ProtoUtils.fromProto(operation.getPrevTS()),
               ProtoUtils.fromProto(operation.getReplicaTS()),
               operation.getReplicaId());
-          case OP_TRANSFER_TO -> new TransferOp(
+      case OP_TRANSFER_TO -> op =
+          new TransferOp(
               operation.getUserId(),
               operation.getDestUserId(),
               operation.getAmount(),
               ProtoUtils.fromProto(operation.getPrevTS()),
               ProtoUtils.fromProto(operation.getReplicaTS()),
               operation.getReplicaId());
-          default -> throw new IllegalArgumentException(PARSE_FAILED);
-        };
+      default -> throw new IllegalArgumentException(PARSE_FAILED);
+    }
 
     // May have failed already on the other server
     if (operation.getFailed()) {
